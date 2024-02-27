9bb76049472d0d7916e28f084439dc699ce4f892
HkhCode <hkh.2003.1381@gmail.com>
2024-02-27 11:00:11 -0800

    Hello world1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HelloWorld from '../APIs/HelloWorld'
 
 function App()
 {
@@ -9,7 +8,7 @@ function App()
 
   return (
     <>
-
+      <HelloWorld />
     </>
   )
 }
