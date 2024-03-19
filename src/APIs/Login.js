33f9cc9f481326d3ed231a3addc27c8886ab2a27
HkhCode <hkh.2003.1381@gmail.com>
2024-03-19 10:06:04 -0700

    Fixed Login

---
+++ src/APIs/Login.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const LoginRequest = (username , password) => {
+    const baseurl = "https://gheychi-api.comp-iust.ir";
+    let data = null;
+    axios.post(baseurl+"/auth/login" , {username :username , password : password}).then(resp => data = resp.data);
+    return data;
+}
+
+export default LoginRequest;
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "../styles/login.module.css";
+import LoginRequest from "../APIs/Login";
 const Login = () => {
+    const [username , setUsername] = useState();
+    const [password , setPassword] = useState();
+    const submitLogin = () => {
+        let token = LoginRequest(username , password);
+        localStorage.setItem("token" , token);
+    }
     return (
         <React.Fragment>
             <div className={style.loginbox}>
@@ -8,9 +15,9 @@ const Login = () => {
                     ورود
                 </div>
                 <div className={style.inputsbox}>
-                    <input type="email" id="email" placeholder="ایمیل"/>
-                    <input type="password" id="password" placeholder="کلمه عبور"/>
-                    <button type="submit" >ورود</button>
+                    <input type="text" id="username" placeholder="نام کاربری" onChange={(e) => setUsername(e.target.value)}/>
+                    <input type="password" id="password" placeholder="کلمه عبور" onChange={(e) => setPassword(e.target.value)}/>
+                    <button type="submit" onClick={submitLogin}>ورود</button>
                 </div>
             </div>
         </React.Fragment>
