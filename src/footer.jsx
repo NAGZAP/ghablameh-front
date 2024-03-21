320cafd317c3666f8229b4d74bacbb22297fc15b
Amir Hossein Raeghi <94464208+lAmiR1HosseiNl@users.noreply.github.com>
2024-03-21 07:55:47 +0330

    "Create&Update(Register):Create Header , Footer , Form"

---
+++ src/footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+
+return(
+    <footer>
+        <p>&copy; {new Date().getFullYear()} Ghablameh </p>
+    </footer>
+    );
+};
+export default Footer
---
+++ src/header.jsx
@@ -0,0 +1,18 @@
+function  Header(){
+
+
+    return (
+        <header>
+            <nav className="zone blue">
+                <ul className="main_nav">
+                    <li><a className="header_texts khane" href = "#">خانه</a></li>
+                    <li><a className="header_texts Service" href = "#">سرویس</a></li>
+                    <li><a className="header_texts About" href = "#">درباره ما</a></li>
+                    <li><a className="header_texts Ertebat" href = "#">ارتباط با ما</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export  default Header
