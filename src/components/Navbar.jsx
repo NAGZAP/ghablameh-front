3a80841937a99585368e63852c96c1ba13022509
MahyaCheraghi <mahya_ch@yahoo.com>
2024-03-29 16:22:42 +0330

    refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Avatar from 'react-avatar';
 import '../styles/Navbar.css';
+// import isLoggedIn from '../APIs/AuthManager'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const dropdownRef = useRef(null);
 
+  //dropdown
   useEffect(() => {
     document.addEventListener('click', handleClickOutside);
     return () => {
@@ -18,8 +21,16 @@ function Navbar() {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const handleClickOutside = (e) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      setIsDropdownOpen(false);
+    }
+  };
+
+  //log in log out
   const handleLogin = () => {
     setIsLoggedIn(true);
+
   };
 
   const handleLogout = () => {
@@ -27,81 +38,75 @@ function Navbar() {
     setIsDropdownOpen(false); // Close the dropdown menu on logout
   };
 
-  // const handleUserPanelClick = () => { 
-
-  //   // if (isLoggedIn) { 
-  //   window.location.href = '../public/index.html'; 
-  //   handleLogin() 
-      
-  //   // } else {
-  //   //   window.location.href = '../public/index.html'; 
-  //   //   handleLogout() 
-
-  //   //   };
-  // };
-
-  const handleClickOutside = (e) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
-      setIsDropdownOpen(false);
-    }
-  };
-
   function UserProfile() {
-    if (isLoggedIn) {
+    if (!isLoggedIn) {
       return (
-        <div className="user-profile flex items-center">
+        <div className="user-profile flex items-center p-1">
           <Avatar onClick={toggleDropdown} name="emma elise" size="60" round={true} maxInitials={1} />
-          {/* <h6 className="text-white vazir" style={{ marginLeft: '15px', fontSize: '20px' }}>emma elise</h6> */}
-              
         </div>
       );
-    } else {
-      return (
-        <button onClick={handleLogin} className="login-button">Login / Sign Up</button>
-      );
     }
   }
-function Usernamee(){
-  if (isLoggedIn) {
-    return (
-    <h6 className="text-white vazir" style={{ marginLeft: '15px', fontSize: '20px' }}>emma elise</h6>
-    )          
+
+  function Username() {
+    if (!isLoggedIn) {
+      return (
+        <h6 className="text-white vazir" style={{ marginLeft: '15px', fontSize: '20px' }}>emma elise</h6>
+      )
+    }
   }
+
+  function LogInButton() {
+    if (isLoggedIn) {
+      return (
+        <div className='flex justify-between items-center'>
+          <div style={{ marginRight: '10px' }}><Link to="/" className='text-white' style={{ fontSize: '18px', fontFamily: 'vazir' }}>وارد شوید</Link></div>
+          <div><h6 className='text-white' style={{ fontSize: '23px', fontFamily: 'vazir' , marginRight: '10px' }}>/</h6></div>
+          <div><Link to="/" className='text-white' style={{ fontSize: '18px', fontFamily: 'vazir' }}>ثبت نام کنید</Link></div>
+        </div>
+      );
+    }
   }
 
   return (
-    <nav className="navbar-design border">
+    <nav className="navbar-design ">
       <div className="px-3">
         <div className="flex justify-between m-2 items-center">
           <div className="flex items-center">
+
             <div ref={dropdownRef}>
+              {/* Avatar, username and Login button */}
               <div className="flex justify-between items-center">
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" type="button">
-                  {UserProfile()}
-                </button>
-                {Usernamee()}
-                </div>
-              <div id="dropdownHover" className={`absolute z-10 ${isDropdownOpen ? '' : 'hidden'} dropdown-me divide-y rounded-lg shadow w-44 dark:bg-gray-700`}>
-                <ul className="py-2 text-sm text-white" aria-labelledby="dropdownDefaultButton">
+                {UserProfile()}
+                {Username()}
+                {LogInButton()}
+              </div>
+
+              {/* Dropdown */}
+              <div className={`absolute z-10 ${isDropdownOpen ? '' : 'hidden'} rounded-lg shadow dropdown-me`}>
+                <ul className="py-1 text-sm text-white">
                   <li><a className="block px-4 py-2 hover:bg-gray-800 dark:hover:bg-gray-600">userpanel</a></li>
-                  <li><a href="../public/index.html" onClick={handleLogout} className="block px-4 py-2 hover:bg-gray-800 dark:hover:bg-gray-600">Logout</a></li>
+                  <li><a href="#" onClick={handleLogout} className="block px-4 py-2 hover:bg-gray-800 dark:hover:bg-gray-600">Logout</a></li>
                 </ul>
               </div>
             </div>
           </div>
+
+          {/* Elements - Logo */}
           <div className="flex items-center justify-end">
             <div className="flex items-center justify-end space-x-3 elements-me">
-              <a href="#" id="GFG" className="fantasyfont text-white hover:text-blue-900" style={{ fontSize: '20px' }}>element1</a>
-              <a href="#" id="GFG" className="fantasyfont text-white hover:text-blue-900" style={{ fontSize: '20px' }}>element2</a>
+              <a href="../public/index.html" id="GFG" className="text-white hover:text-blue-900" style={{ fontSize: '20px' }}>element1</a>
+              <a href="../public/index.html" id="GFG" className="text-white hover:text-blue-900" style={{ fontSize: '20px' }}>element2</a>
             </div>
             <div className="flex justify-end">
-              <a href="#" className="items-center text-white farsifont" style={{ fontSize: '35px', fontFamily: 'vazir' }} id="GFG">قابلمه</a>
+              <a href="../public/index.html" className="items-center text-white " style={{ fontSize: '35px', fontFamily: 'vazir' }} id="GFG">قابلمه</a>
             </div>
           </div>
+
         </div>
       </div>
     </nav>
   );
 }
 
-export default Navbar;
+export default Navbar; 
