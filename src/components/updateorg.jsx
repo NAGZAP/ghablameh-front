dc3dc9e8f4c59a26d9fd821a6a5086b78b29e69c
MahyaCheraghi <mahya_ch@yahoo.com>
2024-04-03 07:07:57 +0330

    fix

--- src/components/updateorg.jsx
+++ src/components/updateorg.jsx
@@ -1,10 +1,24 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
 import '../styles/updateorg.css';
 import Avatar from 'react-avatar';
 import axios from 'axios';
 
 function Updateorg() {
-    const [showMyModel, setShowMyModel] = useState(false);
+
+    //fetch user data
+    const [userData, setUserData] = useState(null);
+    useEffect(() => {
+        const fetchUserData = async () => {
+          try { //https://ghablameh.fiust.ir/api/v1/swagger/?format=openapi#/definitions/Organization/me
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+            setUserData(response.data);
+          } catch (error) {
+            console.error('Error fetching user data: ', error);
+          }
+        };
+        fetchUserData();
+      }, []);
 
     //pic
     const [imagePreview, setImagePreview] = useState('');
@@ -24,6 +38,8 @@ function Updateorg() {
     };
 
     // model
+    const [showMyModel, setShowMyModel] = useState(false);
+
     const onClose = () => {
         setShowMyModel(false);
     };
@@ -37,12 +53,12 @@ function Updateorg() {
         return (
             <div className="flex justify-center mt-4">
                 <label htmlFor="fileInput" className="relative w-20 h-20 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 flex items-center justify-center cursor-pointer">
-                    <input type="file" id="fileInput" ref={fileInputRef} className="hidden" onChange={displayImage} accept="image/*" />
+                    <input name='image' type="file" id="fileInput" ref={fileInputRef} className="hidden" onChange={displayImage} accept="image/*" />
                     {imagePreview ? (
                         <div className="w-full h-full" style={{ backgroundImage: `url(${imagePreview})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
                     ) : (
                         <div className="user-profile flex items-center">
-                            <Avatar name="emma elise" size="60" round={true} maxInitials={1} />
+                            <Avatar name={userData.name} size="80" round={true} maxInitials={1} />
                         </div>
                     )}
                 </label>
@@ -56,44 +72,58 @@ function Updateorg() {
 
                 <div className="sm:col-span-2 p-2 ">
                     <label className="block mb-2 text-sm text-gray-90 text-right"> نام سازمان </label>
-                    <input name="name" id="name" className=" input-me text-gray-900 rounded-md block w-full p-2.5" placeholder=" نام سازمان " />
+                    <input  name="name" id="name" className=" input-me text-gray-900 rounded-md block w-full p-2.5" placeholder=" نام سازمان " />
                 </div>
                 <div className="sm:col-span-2">
-                    <label className="block mb-2 text-sm text-gray-90 text-right">نام کاربری سازمان</label>
-                    <input name="name" id="name" className=" input-me text-gray-900 rounded-md block w-full p-2.5" placeholder="نام کاربری" />
+                    <label className="block mb-2 text-sm text-gray-90 text-right">نام مدیر سازمان</label>
+                    <input name="admin_name" id="name" className=" input-me text-gray-900 rounded-md block w-full p-2.5" placeholder="نام کاربری" />
                 </div>
+                <div className="sm:col-span-2 p-2 ">
+                    <label className="block mb-2 text-sm text-gray-90 text-right"> نام کاربری مدیر سازمان </label>
+                    <input  name="admin_username" id="name" className=" input-me text-gray-900 rounded-md block w-full p-2.5" placeholder=" نام سازمان " />
+                </div>
+                <div className="sm:col-span-2 p-2 ">
+                    <label className="block mb-2 text-sm text-gray-90 text-right"> ایمیل مدیر سازمان</label>
+                    <input  name="email" id="name" className=" input-me text-gray-900 rounded-md block w-full p-2.5" placeholder=" نام سازمان " />
+                </div>
+                
 
-                <div className="sm:col-span-2">
-                    <label className="block mb-2 text-sm text-gray-90 text-right">  ایمیل </label>
-                    <input name="name" id="name" className=" input-me text-gray-900 rounded-md block w-full p-2.5" placeholder=" ایمیل " />
-                </div>
             </div>
         );
     }
 
-    function userPassword() {
-        return (
-            <div className='flex justify-center flex-col'>
-                <h3 className='flex justify-center p2 mb-1'>تغییر رمز عبور</h3>
+    function PasswordFields() {
+    const [showPasswordFields, setShowPasswordFields] = useState(false);
 
-                <div className="border-t my-4"></div>
-                <div className="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5 p-2">
-                    <div className="sm:col-span-2">
-                        <label className="block mb-2 text-sm text-gray-90 text-right">رمز عبور قبلی</label>
-                        <input name="name" id="name" className="input-me text-gray-900 rounded-md block w-full p-2.5" placeholder="رمز عبور قبلی" />
-                    </div>
-                    <div className="sm:col-span-2">
-                        <label className="block mb-2 text-sm text-gray-90 text-right">رمز عبور جدید</label>
-                        <input name="name" id="name" className="input-me text-gray-900 rounded-md block w-full p-2.5" placeholder="رمز عبور جدید" />
-                    </div>
-                    <div className="sm:col-span-2">
-                        <label className="block mb-2 text-sm text-gray-90 text-right">تکرار رمز عبور جدید</label>
-                        <input name="name" id="name" className="input-me text-gray-900 rounded-md block w-full p-2.5" placeholder="تکرار رمز عبور جدید" />
-                    </div>
-                </div>
+    const togglePasswordFields = () => {
+        setShowPasswordFields(!showPasswordFields);
+    };
+
+    return (
+        <div className='flex justify-center flex-col'>
+            <div className='flex justify-center'>
+            <h3 className='flex justify-center p2 mb-1 changepassbutton text-base' onClick={togglePasswordFields}>تغییر رمز عبور</h3>
             </div>
-        );
-    }
+                    <div className="border-t my-4"></div>
+                    <div className="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5 p-2">
+                        <div className="sm:col-span-2">
+                            <label className="block mb-2 text-sm text-gray-90 text-right">رمز عبور قبلی</label>
+                            <input  name="oldPassword" id="oldPassword" className="input-me text-gray-900 rounded-md block w-full p-2.5" placeholder="رمز عبور قبلی" />
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label className="block mb-2 text-sm text-gray-90 text-right">رمز عبور جدید</label>
+                            <input  name="newPassword" id="newPassword" className="input-me text-gray-900 rounded-md block w-full p-2.5" placeholder="رمز عبور جدید" />
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label className="block mb-2 text-sm text-gray-90 text-right">تکرار رمز عبور جدید</label>
+                            <input  name="confirmPassword" id="confirmPassword" className="input-me text-gray-900 rounded-md block w-full p-2.5" placeholder="تکرار رمز عبور جدید" />
+                        </div>
+                        
+                    </div>
+
+        </div>
+    );
+}
 
     return (
         <div>
@@ -108,19 +138,19 @@ function Updateorg() {
                             <button onClick={onClose} className='close-button-me text-sm'>X</button>
                         </div>
 
-                        <div className="max-w-2xl px-4 py-8  lg:py-16">
-                            <h2 className="mb-2 text-xl font-bold text-gray-900 dark:text-white text-center">ویرایش اطلاعات</h2>
-                            <form action="#" className='border-t'>
+                        <div className="form-container" style={{ height: "600px", overflowY: "scroll" }}>
+                            <div className="max-w-2xl px-4 py-8 lg:py-16">
+                                <h2 className="mb-2 text-xl font-bold text-gray-900 dark:text-white text-center">ویرایش اطلاعات</h2>
+                                <form  className='border-t'>
+                                    {userPicuter()}
+                                    {userInfo()}
+                                    <PasswordFields />
 
-                                {userPicuter()}
-                                {userInfo()}
-                                {userPassword()}
-
-                                <div className="flex items-center justify-center space-x-4">
-                                    <button type="submit" className="text-white text-center submit-button-me">ذخیره</button>
-                                </div>
-
-                            </form>
+                                    <div className="flex items-center justify-center space-x-4">
+                                        <button type="submit" className="text-white text-center submit-button-me">ذخیره</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </section>
                 </div>
