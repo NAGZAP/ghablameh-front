aae171ea7b2f66ae50bfd5da5159d44f75bbc9f2
MahyaCheraghi <mahya_ch@yahoo.com>
2024-04-05 20:39:38 +0330

    refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect, useRef } from 'react';
 import Avatar from 'react-avatar';
-import '../styles/Navbar.css';
+import styles from '../styles/Navbar.module.css';
 import isLoggedIn from '../APIs/AuthManager'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-// import Router from 'next/router';
 
 function Navbar() {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -59,7 +58,7 @@ function Navbar() {
   function UserAvatar() {
     if (isLoggedIn) {
       return (
-        <div className="flex items-center p-1">
+        <div className={`flex items-center p-1`}>
           {userData && userData.profilePicture ? (
             <img src={userData.profilePicture} alt="Profile" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />
           ) : (
@@ -69,11 +68,12 @@ function Navbar() {
       );
     }
   }
+
   function Username() {
     if (isLoggedIn && userData) {
       return (
         //{userData.first_name} {userData.last_name}
-        <h6 className="text-white vazir" style={{ marginLeft: '15px', fontSize: '20px' }}>{userData.name}</h6>
+        <h6 className={`${styles['vazir']} text-white`} style={{ marginLeft: '15px', fontSize: '20px' }}>{userData.name}</h6>
       );
     }
   }
@@ -81,24 +81,24 @@ function Navbar() {
   function LogInButton() {
     if (!isLoggedIn) {
       return (
-        <div className='flex justify-between items-center'>
-          <div style={{ marginRight: '10px' }}><Link to="/" className='text-white' style={{ fontSize: '18px', fontFamily: 'vazir' }}>وارد شوید</Link></div>
-          <div><h6 className='text-white' style={{ fontSize: '23px', fontFamily: 'vazir', marginRight: '10px' }}>/</h6></div>
-          <div><Link to="/" className='text-white' style={{ fontSize: '18px', fontFamily: 'vazir' }}>ثبت نام کنید</Link></div>
+        <div className={`flex justify-between items-center`}>
+          <div style={{ marginRight: '10px' }}><Link to="/" className={`text-white`} style={{ fontSize: '18px', fontFamily: 'vazir' }}>وارد شوید</Link></div>
+          <div><h6 className={`text-white`} style={{ fontSize: '23px', fontFamily: 'vazir', marginRight: '10px' }}>/</h6></div>
+          <div><Link to="/" className={`text-white`} style={{ fontSize: '18px', fontFamily: 'vazir' }}>ثبت نام کنید</Link></div>
         </div>
       );
     }
   }
 
   return (
-    <nav className="navbar-design ">
-      <div className="px-3">
-        <div className="flex justify-between m-2 items-center">
-          <div className="flex items-center">
+    <nav className={`${styles['navbar-design']}`}>
+      <div className={`px-3`}>
+        <div className={`flex justify-between m-2 items-center`}>
+          <div className={`flex items-center`}>
 
             <div ref={dropdownRef}>
               {/* Avatar, username and Login button */}
-              <div className="flex justify-between items-center">
+              <div className={`flex justify-between items-center`}>
                 {userData && (
                   <>
                     {UserAvatar()}
@@ -111,23 +111,23 @@ function Navbar() {
               </div>
 
               {/* Dropdown */}
-              <div className={`absolute z-10 ${isDropdownOpen ? '' : 'hidden'} rounded-lg shadow dropdown-me`}>
-                <ul className="py-1 text-sm text-white">
-                  <li><a className="block px-4 py-2 hover:bg-gray-800 dark:hover:bg-gray-600">userpanel</a></li>
-                  <li><a onClick={handleLogout} className="block px-4 py-2 hover:bg-gray-800 dark:hover:bg-gray-600">Logout</a></li>
+              <div className={`absolute z-10 ${isDropdownOpen ? '' : 'hidden'} rounded-lg shadow ${styles['dropdown-me']}`}>
+                <ul className={`py-1 text-sm text-white`}>
+                  <li><a className={`block px-4 py-2 hover:bg-gray-800 dark:hover:bg-gray-600`}>userpanel</a></li>
+                  <li><a onClick={handleLogout} className={`block px-4 py-2 hover:bg-gray-800 dark:hover:bg-gray-600`}>Logout</a></li>
                 </ul>
               </div>
             </div>
           </div>
 
           {/* Elements - Logo */}
-          <div className="flex items-center justify-end">
-            <div className="flex items-center justify-end space-x-3 elements-me">
-              <a id="GFG" className="text-white hover:text-blue-900" style={{ fontSize: '20px' }}>element1</a>
-              <a id="GFG" className="text-white hover:text-blue-900" style={{ fontSize: '20px' }}>element2</a>
+          <div className={`flex items-center justify-end`}>
+            <div className={`flex items-center justify-end space-x-3 ${styles['elements-me']}`}>
+              <a id="GFG" className={`text-white hover:text-blue-900`} style={{ fontSize: '20px' }}>element1</a>
+              <a id="GFG" className={`text-white hover:text-blue-900`} style={{ fontSize: '20px' }}>element2</a>
             </div>
-            <div className="flex justify-end">
-              <a className="items-center text-white " style={{ fontSize: '35px', fontFamily: 'vazir' }} id="GFG">قابلمه</a>
+            <div className={`flex justify-end`}>
+              <a className={`items-center text-white`} style={{ fontSize: '35px', fontFamily: 'vazir' }} id="GFG">قابلمه</a>
             </div>
           </div>
 
