fd1550ffd8e576a87fd3233ba7cd827a3c728983
HkhCode <hkh.2003.1381@gmail.com>
2024-04-04 19:01:29 -0700

    Fix2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,37 +1,79 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import style from './Sidebar.module.css';
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import {
+  HiArrowSmRight,
+  HiChartPie,
+  HiInbox,
+  HiShoppingBag,
+  HiTable,
+  HiUser,
+  HiViewBoards,
+} from "react-icons/hi";
+import { Flowbite } from "flowbite-react";
 const CustomSidebar = () => {
-return (<>
-   <Sidebar aria-label="Default sidebar example" className="bg-blue-900">
-      <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie} className="text-sky-50">
-            داشبورد
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiViewBoards} label="Pro" labelColor="dark" className="text-sky-50">
-            تست
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiInbox} label="3" className="text-sky-50">
-            تست
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiUser} className="text-sky-50">
-          تست
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiShoppingBag} className="text-sky-50">
-          تست
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiArrowSmRight} className="text-sky-50">
-            <Link to="/Login">ورود</Link>
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiTable} className="text-sky-50">
-          <Link to="/Signup">ثبت نام</Link>
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
-    </Sidebar>
-</>)
-}
+  const customTheme = {
+    span: {
+      color: {
+        primary: "bg-red-500 hover:bg-red-600",
+      },
+    },
+  }
+  return (
+    <>
+      <Flowbite theme={customTheme}>
+        <Sidebar aria-label="Default sidebar example">
+          <Sidebar.Items>
+            {/* Modify the className prop to change background color */}
+            <Sidebar.ItemGroup className="bg-orange-500">
+              {" "}
+              {/* Change bg-orange-500 to your desired color */}
+              <Sidebar.Item href="#" icon={HiChartPie} className="text-sky-50">
+                داشبورد
+              </Sidebar.Item>
+              <Sidebar.Item
+                href="#"
+                icon={HiViewBoards}
+                label="Pro"
+                labelColor="dark"
+                className="text-sky-50"
+              >
+                تست
+              </Sidebar.Item>
+              <Sidebar.Item
+                href="#"
+                icon={HiInbox}
+                label="3"
+                className="text-sky-50"
+              >
+                تست
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiUser} className="text-sky-50">
+                تست
+              </Sidebar.Item>
+              <Sidebar.Item
+                href="#"
+                icon={HiShoppingBag}
+                className="text-sky-50"
+              >
+                تست
+              </Sidebar.Item>
+              <Sidebar.Item
+                href="#"
+                icon={HiArrowSmRight}
+                className="text-sky-50"
+              >
+                {/* <Link to="/Login">ورود</Link> */}
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiTable} className="text-sky-50">
+                {/* <Link to="/Signup">ثبت نام</Link> */}
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+      </Flowbite>
+    </>
+  );
+};
 
 export default CustomSidebar;
