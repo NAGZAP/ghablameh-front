0befe2e19461c109bb83a747d37aca88fc452667
MahyaCheraghi <mahya_ch@yahoo.com>
2024-04-18 23:07:19 +0330

    refactor

--- src/BoofehsOfOrganization/Boofeh.jsx
+++ src/BoofehsOfOrganization/Boofeh.jsx
@@ -1,16 +1,28 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import styles from "./Boofeh.module.css";
-import AddBuffet from '../components/addbuffet';
-import { useModalState } from '../components/modalState';
 
 function Boofeh({ searchTerm, onSearchChange }) {
-  const { showMyModel, setShowMyModel } = useModalState();
-  
+  const [showMyModel, setShowMyModel] = useState(false); // Initialize showMyModel state to false
+    
+  // Model
+  const onClose = () => {
+    setShowMyModel(false);
+  };
+
+  const handleOnClose = (e) => {
+    if (e.target.id === "close") onClose();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
+    <div className='flex flex-col ' >
       <form className="mt-52 max-w-md mx-auto items-center">
         <div className={styles.search}>
-          <label htmlFor="default-search" className="text-sm font-medium text-template-custom-white sr-only dark:text-template-custom-white">
+          <label htmlFor="default-search" className="text-sm font-medium text-template-custom-white sr-only dark:text-template-custom-white" style={{width:'30rem'}}>
             Search
           </label>
           <div className="relative">
@@ -31,16 +43,34 @@ function Boofeh({ searchTerm, onSearchChange }) {
         </div>
       </form>
 
-      {/* <div className="justify-center mt-0 mb-10 flex flex-col items-center">
-        <button onClick={AddBuffet.onOpen} className={styles.button} type="button">
-          <a href="#">افزودن بوفه</a>
-        </button>
-      </div> */}
       <button onClick={() => setShowMyModel(true)} className={styles.button} type="button">
-     <a href="#">افزودن بوفه</a>
-   </button>
-      {showMyModel && <AddBuffet />}
+        <a href="#">افزودن بوفه</a>
+      </button>
+      </div>
+      {showMyModel && (
+        <div id='close' onClick={handleOnClose} className={`${styles['modal-me']} fixed `}>
+          <div className={`bg-white rounded p-2 ${styles['modal-content']}`} >
+            <div className='flex flex-row justify-end'>
+              <button onClick={onClose} className={`${styles['close-button-me']} text-sm`}>X</button>
+            </div>
 
+            <div style={{ width: "400px" }}>
+              <div className="max-w-2xl px-4 ">
+                <h2 className="mb-2 text-xl font-bold text-gray-900 text-center">افزودن بوفه</h2>
+                <form onSubmit={handleSubmit} className={`${styles['border-t']}`}>
+                  <div className="sm:col-span-2 p-3">
+                    <label className={`${styles['text-right']} block mb-2 text-sm text-gray-90`}>نام بوفه</label>
+                    <input name="admin_phone_number" className="text-gray-900 rounded-md block w-full p-2.5" style={{ border: '1px solid #000000' }}  />      
+                  </div>
+                  <div className="flex items-center justify-center space-x-4">
+                    <button type="submit" className={`${styles['submit-button-me']} text-white text-center`}>ذخیره</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
--- src/components/addbuffet.jsx
+++ src/components/addbuffet.jsx
@@ -23,31 +23,7 @@ function AddBuffet() {
             {/* <button onClick={() => setShowMyModel(true)} className={`${styles['update-button-me']} text-white rounded text-sm text-center`} type="button"> افزودن بوفه </button> */}
 
             {/* Main modal */}
-            {showMyModel && (
-                <div id='close' onClick={handleOnClose} className={`${styles['modal-me']} fixed `}>
-                    <div className={`bg-white rounded p-2 ${styles['modal-content']}`} >
-                        <div className='flex flex-row justify-end'>
-                            <button onClick={onClose} className={`${styles['close-button-me']} text-sm`}>X</button>
-                        </div>
-
-                        <div style={{ width: "400px" }}>
-                            <div className="max-w-2xl px-4 ">
-                                <h2 className="mb-2 text-xl font-bold text-gray-900 text-center"> افزودن بوفه </h2>
-                                <form onSubmit={handleSubmit} className={`${styles['border-t']}`}>
-                                    <div className="sm:col-span-2 p-3">
-                                        <label className={`${styles['text-right']} block mb-2 text-sm text-gray-90`}> نام بوفه </label>
-                                        <input name="admin_phone_number" className="text-gray-900 rounded-md block w-full p-2.5" style={{ border: '1px solid #000000' }}  />
-                                        
-                                    </div>
-                                    <div className="flex items-center justify-center space-x-4">
-                                        <button type="submit" className={`${styles['submit-button-me']} text-white text-center`}>ذخیره</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )}
+            m
         </div>
     );
 }
