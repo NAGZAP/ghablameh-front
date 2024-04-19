17f2e6d3db4732577360345ede9240ae2ad6ad9e
Parrnia <117802518+Parrnia@users.noreply.github.com>
2024-04-19 06:47:05 -0700

    add input

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import styles from  '../styles/input.module.css';
 
-const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+
 const InputField = () => {
   return (
     <div>
-    <div className={styles.pattern}>
+    <div className={styles.pattern}> 
       <label htmlFor="email" className={styles.label}>
             input
             </label>
