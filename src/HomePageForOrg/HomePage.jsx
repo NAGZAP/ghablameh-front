7ff1d7bd14b432fa26265eb7628c71a9dcd79c3f
lAmiR1HosseiNl <araeghi@gmail.com>
2024-05-01 20:48:57 +0330

    Fix(HomeOrg):Add style and grid

--- src/HomePageForOrg/HomePage.jsx
+++ src/HomePageForOrg/HomePage.jsx
@@ -3,22 +3,19 @@ import Navbar from '../components/Navbar.jsx';
 import styles from './Homepage.module.css'
 function HomeOrgPage() {
     return(
-        <>
-        <Navbar></Navbar>
-        <div className='grid grid-cols-5 grid-rows-12'>
-            <div className='lg:col-start-2 lg:col-span-3 lg:row-span-12'>
-                <div className={styles.Boofehs_slide_card}>
-                    Boofeh
+        <div className={styles.containment_OrgHome}>
+            <div className={styles.itemscenter}>    
+                <Navbar></Navbar>
+                <div className='grid grid-cols-5 grid-rows-12'>
+                    <div className='lg:col-start-2 lg:col-span-3 lg:row-span-12'>
+                        <div className='bg-white'>
+                            Boofeh
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-
-
-
-            <div className={styles.footer}>
                 <Footer></Footer>
             </div>
-        </>
+        </div>
     );
 };
 export default HomeOrgPage;
