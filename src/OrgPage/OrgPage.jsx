cc09f9902283bf69a56ad48e5ec8a5624f169207
lAmiR1HosseiNl <araeghi@gmail.com>
2024-05-01 19:34:28 +0330

    Add(OrgPage):add modale for delete to ensure the user want to delete

--- src/OrgPage/OrgPage.jsx
+++ src/OrgPage/OrgPage.jsx
@@ -5,22 +5,18 @@ import Footer from '../components/footer';
 import Navbar from '../components/Navbar.jsx';
 
 function OrgPage() {
-/*   const [peopleData, setPeopleData] = useState([
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Doe' },
-    { id: 3, name: 'Alice Smith' },
-  ]); */
-   const [peopleData, setPeopleData] = useState([]);
-   
+  const [peopleData, setPeopleData] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [newBuffetName, setNewBuffetName] = useState('');
   const [editBuffet, setEditBuffet] = useState(null);
   const [editBuffetName, setEditBuffetName] = useState('');
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+  const [deleteBuffetId, setDeleteBuffetId] = useState(null);
 
   // Define your API endpoint and token
   const API_ENDPOINT = 'https://ghablameh.fiust.ir/api/v1/buffets/';
   const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE3MTYxODEzLCJpYXQiOjE3MTQ1Njk4MTMsImp0aSI6ImQxZTM5Y2I4Yzk3ODQxMGFiYjA0NTVkN2U2M2QwMWUwIiwidXNlcl9pZCI6Mn0.CUEgrbFFFKgk5sy7VfqlKaWVqqg5Gv6hSbDZdrIbetA';
-  
+
   useEffect(() => {
     // Axios request configuration
     const axiosConfig = {
@@ -39,10 +35,16 @@ function OrgPage() {
       .catch(error => {
         console.error('Error fetching data:', error);
       });
-  }, []); // Empty dependency array ensures the effect runs only once, similar to componentDidMount
-  const handleDelete = (id) => {
+  }, []);
+
+  const handleDeleteConfirmation = (id) => {
+    setDeleteBuffetId(id);
+    setDeleteConfirmation(true);
+  };
+
+  const handleDelete = () => {
     // Define your delete endpoint
-    const deleteEndpoint = `${API_ENDPOINT}${id}/`;
+    const deleteEndpoint = `${API_ENDPOINT}${deleteBuffetId}/`;
 
     // Axios request configuration for delete operation
     const axiosConfig = {
@@ -56,7 +58,9 @@ function OrgPage() {
     axios.delete(deleteEndpoint, axiosConfig)
       .then(response => {
         // If delete is successful, update state to remove the deleted entity
-        setPeopleData(prevData => prevData.filter(person => person.id !== id));
+        setPeopleData(prevData => prevData.filter(person => person.id !== deleteBuffetId));
+        // Close the delete confirmation modal
+        setDeleteConfirmation(false);
       })
       .catch(error => {
         console.error('Error deleting data:', error);
@@ -68,7 +72,7 @@ function OrgPage() {
     const postData = {
       name: newBuffetName
     };
-  
+
     // Axios request configuration for create operation
     const axiosConfig = {
       headers: {
@@ -77,7 +81,7 @@ function OrgPage() {
         // Add other headers as needed
       }
     };
-  
+
     // Send POST request using Axios
     axios.post(API_ENDPOINT, postData, axiosConfig)
       .then(response => {
@@ -91,7 +95,7 @@ function OrgPage() {
         console.error('Error creating buffet:', error);
       });
   };
-  
+
   const handleEdit = () => {
     // Define the endpoint for editing the buffet
     const editEndpoint = `${API_ENDPOINT}${editBuffet.id}/`;
@@ -147,11 +151,11 @@ function OrgPage() {
                             {editBuffet ? 'ویرایش بوفه' : 'ساخت بوفه جدید'}
                           </h3>
                           <div className="mt-2">
-                            <input 
-                              type="text" 
-                              value={editBuffet ? editBuffetName : newBuffetName} 
-                              onChange={(e) => editBuffet ? setEditBuffetName(e.target.value) : setNewBuffetName(e.target.value)} 
-                              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                            <input
+                              type="text"
+                              value={editBuffet ? editBuffetName : newBuffetName}
+                              onChange={(e) => editBuffet ? setEditBuffetName(e.target.value) : setNewBuffetName(e.target.value)}
+                              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                               placeholder="نام بوفه جدید"
                             />
                           </div>
@@ -170,42 +174,69 @@ function OrgPage() {
                 </div>
               </div>
             )}
-            <div className={styles.transform}>
-            <div className='m-10 grid lg:grid-cols-5 md:grid-cols-5  mt-5 mb-5'>
-              <div className='lg:col-start-2 lg:col-span-3 md:col-start-2 md:col-span-3 '>
-              <table className="table-auto min-w-full bg-white bg-opacity-60 rounded-lg divide-y divide-template-custom-orange">
-                <thead>
-                  <tr>
-                    <th className="px-6 py-3  text-xs font-bold text-template-custom-orange uppercase tracking-wider">بوفه</th>
-                    <th className="px-6 py-3  text-xs font-bold text-template-custom-orange uppercase tracking-wider">عملیات</th>
-                    <th className="px-6 py-3  text-xs font-bold text-template-custom-orange uppercase tracking-wider"><button onClick={() => setShowModal(true)} className="m-2  px-4 py-2 font-medium text-white bg-orange-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">ساخت بوفه</button></th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-template-custom-blue">
-                    {peopleData.map(person => (
-                      <tr key={person.id}>
-                        {/* Column 1: Person Name */}
-                        <td className="px-6 py-4 whitespace-nowrap">{person.name}</td>
-                        
-                        {/* Column 2: Operation Buttons */}
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          {/* Edit Button */}
-                          <button onClick={() => { setEditBuffet(person); setEditBuffetName(person.name); setShowModal(true); }} className="px-2 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">ویرایش</button>
-                          
-                          {/* Table Button */}
-                          <button onClick={() => {}} className="ml-2 px-2 py-2 font-medium text-white bg-gray-600 rounded-md hover:bg-gray-500 focus:outline-none focus:shadow-outline-blue active:bg-gray-600 transition duration-150 ease-in-out">جدول</button>
-                          
-                          {/* Delete Button */}
-                          <button onClick={() => handleDelete(person.id)} className="ml-2 px-2 py-2 font-medium text-white bg-orange-600 rounded-md hover:bg-orange-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">حذف</button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
 
-              </table>
+            {/* Delete Confirmation Modal */}
+            {deleteConfirmation && (
+              <div className="fixed z-10 inset-0 overflow-y-auto">
+                <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                  <div className="fixed inset-0 transition-opacity">
+                    <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                  </div>
+                  <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
+                  <div className="inline-block align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                      <div className="sm:flex sm:items-start">
+                        <div className="text-center sm:mt-0 sm:ml-4 sm:text-left">
+                          <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">آیا مطمئن هستید؟</h3>
+                          <div className="mt-2">
+                            <button onClick={handleDelete} className="px-4 py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">حذف</button>
+                            <button onClick={() => setDeleteConfirmation(false)} className="mx-2 px-4 py-2 bg-gray-600 text-white font-semibold rounded-md hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">لغو</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+
+            <div className={styles.transform}>
+              <div className='m-10 grid lg:grid-cols-5 md:grid-cols-5 mt-5 mb-5'>
+                <div className='lg:col-start-2 lg:col-span-3 md:col-start-2 md:col-span-3 '>
+                  <table className="table-auto min-w-full bg-white bg-opacity-60 rounded-lg divide-y divide-template-custom-orange">
+                    <thead>
+                      <tr>
+                        <th className="px-6 py-3 text-xs font-bold text-template-custom-orange uppercase tracking-wider">بوفه</th>
+                        <th className="px-6 py-3 text-xs font-bold text-template-custom-orange uppercase tracking-wider">عملیات</th>
+                        <th className="px-6 py-3 text-xs font-bold text-template-custom-orange uppercase tracking-wider">
+                          <button onClick={() => setShowModal(true)} className="m-2 px-4 py-2 font-medium text-white bg-orange-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">ساخت بوفه</button>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y divide-template-custom-blue">
+                      {peopleData.map(person => (
+                        <tr key={person.id}>
+                          {/* Column 1: Person Name */}
+                          <td className="px-6 py-4 whitespace-nowrap">{person.name}</td>
+
+                          {/* Column 2: Operation Buttons */}
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            {/* Edit Button */}
+                            <button onClick={() => { setEditBuffet(person); setEditBuffetName(person.name); setShowModal(true); }} className="px-2 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">ویرایش</button>
+
+                            {/* Table Button */}
+                            <button onClick={() => {}} className="ml-2 px-2 py-2 font-medium text-white bg-gray-600 rounded-md hover:bg-gray-500 focus:outline-none focus:shadow-outline-blue active:bg-gray-600 transition duration-150 ease-in-out">جدول</button>
+
+                            {/* Delete Button */}
+                            <button onClick={() => handleDeleteConfirmation(person.id)} className="ml-2 px-2 py-2 font-medium text-white bg-orange-600 rounded-md hover:bg-orange-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">حذف</button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
-            </div> 
           </div>
           <Footer />
         </div>
