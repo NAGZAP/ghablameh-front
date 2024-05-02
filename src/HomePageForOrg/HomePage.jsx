205472c4e9d636d82feb107995443e1dce1f510f
lAmiR1HosseiNl <araeghi@gmail.com>
2024-05-02 17:42:14 +0330

    fix(HomePageOrg):add grid

--- src/HomePageForOrg/HomePage.jsx
+++ src/HomePageForOrg/HomePage.jsx
@@ -7,7 +7,7 @@ function HomeOrgPage() {
             <div className={styles.itemscenter}>    
                 <Navbar></Navbar>
                 <div className='grid grid-cols-5 grid-rows-12'>
-                    <div className='lg:col-start-2 lg:col-span-3 lg:row-span-12'>
+                    <div className='col-start-1 col-span-2 lg:row-span-12'>
                         <div className='bg-white'>
                             Boofeh
                         </div>
