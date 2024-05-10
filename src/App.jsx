b08ae5920ce9fafe71b9abb1b39e98b5ce1ad45d
MahyaCheraghi <mahya_ch@yahoo.com>
2024-05-10 15:43:50 +0330

    fix sidebar and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer";
@@ -11,9 +10,6 @@ import Navbar from "./components/Navbar";
 import Updateorg from "./components/updateorg";
 import Login from "./components/login";
 import ListOfJoinRequests from "./components/listofrequests";
-import Boofeh from "./BoofehsOfOrganization/Boofeh";
-import Cards from "./BoofehsOfOrganization/Cards";
-import DataFromApiList from "./BoofehsOfOrganization/DataFromApiList";
 import OrgPage from "./OrgPage/OrgPage";
 import ChooseOrganizationPage from "./pages/ChooseOrganizationPage";
 import Myorgs from "./pages/Myorgs";
@@ -33,10 +29,9 @@ function App() {
         <Route path="/Register" element={<Register />} />
         <Route path="/sidebar" element={<CustomSidebar />} />
         <Route path="/navbar" element={<Navbar />} />
-        <Route path="/" element={<Updateorg />} />
+        <Route path="/Updateorg" element={<Updateorg />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-{/*         <Route path="/DataFromApiList" element={<DataFromApiList />} />*/}    
         <Route path="/HomeOrgPage" element={<HomeOrgPage />} />
         <Route path="/ListOfJoinRequests" element={<ListOfJoinRequests />} />
         <Route path="/OrgPage" element={<OrgPage />} />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,36 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Sidebar } from "flowbite-react";
 import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+
 const CustomSidebar = () => {
-return (<>
-   <Sidebar aria-label=" Default sidebar example" style={{position:'absolute' , right:0 ,zIndex:'3'}} >
+  return (<>
+    <Sidebar aria-label=" Default sidebar example" style={{ position: 'absolute', right: 0, zIndex: '3' }} >
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            داشبورد
+          
+          <Sidebar.Item icon={HiViewBoards}>
+            <Link to='/Update'>تغییر اطلاعات</Link>
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiViewBoards} label="Pro" labelColor="dark">
-            تست
+          <Sidebar.Item icon={HiInbox}>
+            پنل کاربری
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiInbox} label="3">
-            تست
+          <Sidebar.Item icon={HiInbox}>
+            <Link to='/WeeklyMenu'> برنامه غذایی </Link>
           </Sidebar.Item>
-          {/* <Sidebar.Item href="#" icon={HiUser}>
-          تست
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiShoppingBag}>
-          تست
-          </Sidebar.Item> */}
-          <Sidebar.Item href="#" icon={HiArrowSmRight}>
-            <Link to="/Login">ورود</Link>
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiTable}>
-          <Link to="/Signup">ثبت نام</Link>
-          </Sidebar.Item>
+          
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
-</>)
+  </>)
 }
 
 export default CustomSidebar;
