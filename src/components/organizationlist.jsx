81ef4734c6071854abbc6adfc4d8a0cb48a778a5
MahyaCheraghi <mahya_ch@yahoo.com>
2024-06-08 02:07:45 +0330

    refactor navbar and orglist styles

--- src/components/organizationlist.jsx
+++ src/components/organizationlist.jsx
@@ -2,12 +2,22 @@ import axios from 'axios';
 import AuthManager from "../APIs/AuthManager";
 import { useState, useEffect } from "react";
 import Select from 'react-select';
-import defaultPhoto from '../images/organization.jpg'
-
+import defaultPhoto from '../images/team.png'
+import styles from '../styles/organizationlist.module.css'
 function OrganizationList() {
     const [orgData, setOrgData] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [rateFilter, setRateFilter] = useState('');
+    const customStyles = {
+        control: (provided, state) => ({
+            ...provided,
+            borderColor: state.isFocused ? 'rgb(38, 87, 124)' : 'rgb(38, 87, 124)', // Changes the border color when the select is focused and when it is not.
+            borderRadius: '8px',
+            backgroundColor: 'rgba(255, 255, 255, 0.6)', 
+            boxShadow: state.isFocused ? '0 0 0 1px #A593E0' : 'none', // Removes the blue border on focus
+        }),
+        // rest of your custom styles
+    };
 
     useEffect(() => {
         const fetchOrganizations = async () => {
@@ -41,38 +51,43 @@ function OrganizationList() {
     ];
 
     const handleRateFilterChange = (selectedOption) => {
-        setRateFilter(selectedOption.value);
+        setRateFilter(parseInt(selectedOption.value));
     };
 
+    // const filteredOrgData = orgData
+    //     .filter(org => org.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    //     .filter(org => rateFilter ? String(org.average_rate) === rateFilter : true);
     const filteredOrgData = orgData
         .filter(org => org.name.toLowerCase().includes(searchTerm.toLowerCase()))
-        .filter(org => rateFilter ? String(org.average_rate) === rateFilter : true);
-
+        .filter(org => rateFilter ? org.average_rate >= rateFilter && org.average_rate < (rateFilter + 1) : true);
     return (
         <div className="container mx-auto p-4">
             {AuthManager.isLoggedIn() && orgData && (
                 <>
-                    <div className="flex flex-row gap-4 mb-4">
+                    <div className={`flex flex-row gap-4 mb-4 items-center ${styles.wrapper}`}>
                         {/* search */}
                         <input
                             type="text"
                             placeholder="جست و جو سازمان ها ... "
                             value={searchTerm}
                             onChange={handleSearchChange}
-                            className="p-2 border rounded-lg w-full md:w-1/3"
-                            style={{ border: '1px solid rgb(38, 87, 124)' }}
+                            className={`p-2 border rounded-lg w-full md:w-1/3`}
+                            style={{ border: '1px solid rgb(38, 87, 124)',width:'32.7%' }}
                         />
 
                         {/* filter */}
-                        <Select
-                            title="فیلتر بر اساس امتیاز"
-                            value={rateOptions.find(option => option.value === rateFilter)}
-                            onChange={handleRateFilterChange}
-                            options={rateOptions}
-                            className="z-10 md:w-1/6.5"
-                            placeholder="فیلتر بر اساس امتیاز"
-                            isSearchable={false}
-                        />
+                        <div className='' style={{ width: '10vw' }}>
+                            <Select
+                                placeholder="فیلتر بر اساس امتیاز"
+                                value={rateOptions.find(option => option.value === rateFilter)}
+                                onChange={handleRateFilterChange}
+                                options={rateOptions}
+                                className="z-10 md:w-1/7"
+                                title="فیلتر بر اساس امتیاز"
+                                isSearchable={false}
+                                styles={customStyles}
+                            />
+                        </div>
                     </div>
                 </>
             )}
@@ -80,13 +95,13 @@ function OrganizationList() {
             {/* orgs list */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {filteredOrgData.map((org, index) => (
-                    <div key={index} className="bg-white shadow-md rounded-lg overflow-hidden" style={{ border: '1px solid rgb(38, 87, 124)' }}>
+                    <div key={index} className="bg-white bg-opacity-50 shadow-md rounded-lg overflow-hidden" style={{ border: '1px solid rgb(38, 87, 124)' }}>
                         {org.image_url && org.image_url.toLowerCase().endsWith('.jpeg') ? (
-                            <div className="w-full h-48 overflow-hidden" style={{ padding: '10px', background: 'white' }}>
-                            <img src={'https://ghablameh.fiust.ir' + org.image_url} className="w-full h-full" style={{ objectFit: 'contain' }} />
-                          </div>
-                            ) : (
-                            <img src={defaultPhoto} className="w-full h-48 object-cover" />
+                            <div className="bg-white w-full h-48 overflow-hidden bg-opacity-50" style={{ padding: '10px' }}>
+                                <img src={'https://ghablameh.fiust.ir' + org.image_url} className="w-full h-full rounded-lg" style={{ objectFit: 'contain' }} />
+                            </div>
+                        ) : (
+                            <img src={defaultPhoto} className="w-full h-48" style={{ objectFit: 'contain' }} />
                         )}
                         <div className="p-4 flex flex-col justify-start" style={{ borderTop: '1px solid rgb(38, 87, 124)' }}>
                             <h3 className="font-bold text-lg">{org.name}</h3>
